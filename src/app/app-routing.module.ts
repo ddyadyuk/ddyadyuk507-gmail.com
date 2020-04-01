394f8d583c20b19b559fbbf66289d081b1facf50
Dzmitry Dziadziuk <dzmitry_dziadziuk@epam.com>
2020-04-01 10:00:00 +0300

    LP-1: slide drawer has been added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,11 @@ const routes: Routes = [
     {
         path: 'home',
         loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-    }
+    },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+  }
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Course} from './models/course.model';
+import {CoursesService} from './courses/courses.service';
+import {Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+    selector: 'app-home',
+    templateUrl: './home.page.html',
+    styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
+    courses: Course[];
+    isLoading = false;
+    coursesSub: Subscription;
 
-  constructor() { }
+    constructor(
+        private coursesService: CoursesService,
+        private router: Router) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.coursesService.courses.subscribe(courses => {
+            this.courses = courses;
+        });
+    }
+
+    ionViewWillEnter() {
+        this.isLoading = true;
+        this.coursesSub = this.coursesService.fetchCourses().subscribe(courses => {
+            this.courses = courses;
+            this.isLoading = false;
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.coursesSub.unsubscribe();
+    }
 
 }
